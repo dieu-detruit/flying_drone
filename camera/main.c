33112435d40a87aa29ce7b40cf52180e7e82df7a
17Takafumi <dieu.detruit@gmail.com>
2017-08-24 15:09:31 +0900

    gpio c test

---
+++ camera/main.c
@@ -0,0 +1,20 @@
+#include<wiringPi.h>
+
+#define GPIO_LED		25
+
+int main(void)
+{
+
+	if(wiringPiSetupGpio() == -1)return 1;
+
+	pinMode(GPIO_LED, OUTPUT);
+	digitalWrite(GPIO_LED, 1);
+	delay(1000);
+	digitalWrite(GPIO_LED, 0);
+
+	return 0;
+}
+
+
+
+
